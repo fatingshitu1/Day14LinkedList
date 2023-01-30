b5b8a98beed43ab0e000ac5c506e0dd7ce13bd59
shitalfating <shitalfating84@gmail.com>
2023-01-30 17:24:34 +0530

    Added:Welcome to Linked List Program

---
+++ src/com/bridgelabz/LinkedList/LinkedList.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.LinkedList;
+
+public class LinkedList <T> {
+    Node<T> head;
+    Node<T> tail;
+
+    public void push(T data) {
+        Node<T> newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void display() {
+        Node<T> temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+    }
+}
---
+++ src/com/bridgelabz/LinkedList/Node.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.LinkedList;
+
+public class Node <E> {
+
+    E data;
+    Node<E> next;
+
+    public Node (E data) {
+        this.data = data;
+    }
+}

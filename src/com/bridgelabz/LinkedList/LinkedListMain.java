681f09df2fdbeebf758e31867241223fe3950a2c
shitalfating <shitalfating84@gmail.com>
2023-02-01 16:40:04 +0530

    Refactored:Inserting_the_element

--- src/com/bridgelabz/LinkedList/LinkedListMain.java
+++ src/com/bridgelabz/LinkedList/LinkedListMain.java
@@ -8,15 +8,32 @@ public class LinkedListMain {
         linkedList1.push(56);
 
 
-
         linkedList1.display();
-        System.out.println();
-
 
         LinkedList<Integer>LinkedList2 = new LinkedList();
         LinkedList2.append(56);
         LinkedList2.append(30);
         LinkedList2.append(70);
+
+        LinkedList2.display();
+
+        LinkedList<Integer>LinkedList3 = new LinkedList();
+        LinkedList2.push(70);
+        LinkedList2.push(30);
+        LinkedList2.push(56);
+
+        LinkedList3.display();
+        if (LinkedList3.search (40)== null)
+            System.out.println("Element not found !!");
+        else
+            System.out.println("Element Found !!");
+
+        boolean isInserted = LinkedList3.insertAfter(40,30);
+        if (isInserted)
+            System.out.println("Element inserted successfully !!");
+        else
+            System.out.println("Element not inserted!!");
+        LinkedList3.display();
     }
 }
 
